75138f208497560314649963a343c0eeba194d0c
nucular <nucular@users.noreply.github.com>
2016-05-20 23:03:10 +0200

    D: Handle special cases for renamed pages

--- lib/docs/filters/d/entries.rb
+++ lib/docs/filters/d/entries.rb
@@ -1,8 +1,19 @@
 module Docs
   class D
     class EntriesFilter < Docs::EntriesFilter
+      # Pages that are named differently on the index than in their header
+      FIXED_NAMES = {
+        "grammar" => "Grammar", # D Grammar
+        "struct" => "Structs and Unions", # Structs, Unions
+        "errors" => "Error Handling", # Errors
+        "iasm" => "D x86 Inline Assembler", # Inline Assembler
+        "ddoc" => "Embedded Documentation", # Documentation Generator
+        "portability" => "Portability Guide", # Portability
+        "memory-safe-d" => "Memory Safety" # Memory-Safe-D-Spec
+      }
+
       def get_name
-        at_css('h1').inner_text
+        FIXED_NAMES[slug] or at_css('h1').inner_text
       end
 
       def get_type
